adccc16229fc839171b7ad8f7bd58e3c37a83bc9
leomet07 <leomet07@gmail.com>
2021-11-26 19:26:39 -0500

    submit....

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <vector>
+#include <iterator>
+#include <set>
+
 
 using namespace std;
  
@@ -27,44 +29,48 @@ int main() {
 
     long long ins = getLongLong();
     
-    for (long long i = 0; i < 1; i++){
+    for (long long i = 0; i < ins; i++){
         // run
         string s = getString();
 
-        vector<size_t> suses;
-        vector<size_t> ntarsises;
+        // empty set container
+        set<int, greater<int> > rem;
+ 
+        
+        
+        size_t last_ntarsisus = s.find("ntarsisus");
+        // account for ntarsisus
+        while (last_ntarsisus != string::npos){
+            
+            rem.insert(last_ntarsisus + 6);  // to remove the second s in ntarsisus
+            s.replace(last_ntarsisus, 9,"jjjjjjjjj"); // so the sus inside ntarsisus is not found later on
+
+            last_ntarsisus = s.find("ntarsisus", last_ntarsisus + 9); // 9 bc natrsisus is 9 chars long
+        }
+
+        
 
         size_t last_sus = s.find("sus");
         size_t last_ntarsis = s.find("ntarsis");
-
         // find all occurences
-
         while (last_sus != string::npos){
-            cout << "Occurence of sus: " << last_sus + 1 << endl;
-            suses.push_back(last_sus);
+            rem.insert(last_sus + 1); // to remove the u in sus
             last_sus = s.find("sus", last_sus + 3); // 3 bc sus is 3 chars long
         }
         while (last_ntarsis != string::npos){
-            cout << "Occurence of ntarsis: " << last_ntarsis << endl;
-            ntarsises.push_back(last_ntarsis);
+            rem.insert(last_ntarsis + 6);  // to remove the last s in ntarsis
             last_ntarsis = s.find("ntarsis", last_ntarsis + 7); // 7 bc natrsis is 7 chars long
         }
-
-        long long sus_occurences = suses.size();
-        long long ntarsis_occurences = ntarsises.size();
-
-        cout << sus_occurences + ntarsis_occurences << endl;
         
-        for (int i = 0; i < sus_occurences; i++){
-            cout << suses[i] + 1 << " ";
-        }
-        for (int i = 0; i < ntarsis_occurences; i++){
-            cout << ntarsises[i] + 6 << " ";
+
+        cout << rem.size() << endl;
+
+        set<int >::iterator it ;
+        for (it = rem.begin() ; it != rem.end() ; it++ ) {
+            cout << *it<<" ";
         }
 
         cout << endl;
-        
-        
     
     }
     return 0;
