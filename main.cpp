27ac0a41daf23a647916249b30d1ae8085de3ad2
leomet07 <leomet07@gmail.com>
2021-11-26 20:11:56 -0500

    possible accounting for susus as well?

--- main.cpp
+++ main.cpp
@@ -50,13 +50,37 @@ int main() {
             last_ntarsisus = s.find("ntarsisus", last_ntarsisus + 8); // 8 bc natrsisus is 9 chars long and u want to start with latest
         }
 
+        size_t last_susus = s.find("susus");
+        vector<size_t> sflags;
+
+        // account for susus
+        while (last_susus != string::npos){
+            sflags.push_back(last_susus); // flag it
+            rem.insert(last_susus + 2);  // to remove the second s in ntarsisus
+            s.replace(last_susus, 4,"jjjj"); // so the sus inside ntarsisu is not found later on
+
+            last_susus = s.find("susus", last_susus + 4); // 4 bc natrsisus is 5 chars long and u want to start with latest
+        }
+
+
         
 
         size_t last_sus = s.find("sus");
         size_t last_ntarsis = s.find("ntarsis");
         // find all occurences
         while (last_sus != string::npos){
-            rem.insert(last_sus + 1); // to remove the u in sus
+            // check over the flags
+            bool isAccounted = false;
+            for (const auto &item : sflags) {
+                if (item == last_sus) {
+                    cout << "el is present in vector" << endl;
+                    isAccounted = true;
+                    break;
+                }
+            }
+            if (!isAccounted){
+                rem.insert(last_sus + 1); // to remove the u in sus
+            } // else do nothing bc already accounted for
             last_sus = s.find("sus", last_sus + 2); // 2 bc sus is 3 chars long and u want to start with latest
         }
         while (last_ntarsis != string::npos){
@@ -90,31 +114,31 @@ int main() {
 
 
         // DEBUG
-        cout << "\n\nDebug:::::: " << endl;
+        // cout << "\n\nDebug:::::: " << endl;
 
 
-        cout << "S: " << s << endl;
+        // cout << "S: " << s << endl;
 
-        set<int >::iterator d ;
-        for (d = rem.begin() ; d != rem.end() ; d++ ) {
-            cout << *d<<" ";
-            s.erase(*d , 1);
+        // set<int >::iterator d ;
+        // for (d = rem.begin() ; d != rem.end() ; d++ ) {
+        //     cout << *d<<" ";
+        //     s.erase(*d , 1);
             
-        }
-        cout << endl;
+        // }
+        // cout << endl;
 
-        cout << "S new: " << s << endl;
+        // cout << "S new: " << s << endl;
 
-        // find any sus or ntarsis
-        size_t us = s.find("sus");
-        size_t is = s.find("ntarsis");
+        // // find any sus or ntarsis
+        // size_t us = s.find("sus");
+        // size_t is = s.find("ntarsis");
 
-        if (us != string::npos){
-            cout << "sus found: " << us << endl;
-        }
-        if (is != string::npos){
-            cout << "ntarsis found: " << is << endl;
-        }
+        // if (us != string::npos){
+        //     cout << "sus found: " << us << endl;
+        // }
+        // if (is != string::npos){
+        //     cout << "ntarsis found: " << is << endl;
+        // }
 
     
     }
